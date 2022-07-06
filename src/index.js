87ef006d5e18469b0c499f2d286a639da50f7d3f
Deren B <dbozerprogramming@gmail.com>
2022-07-06 03:05:42 -0400

    Final layout

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import image from "./images/profile-picture.jpg";
+import scrimlogo from "./images/logo-react.png";
+import codeLogo from "./images/logo-code.png";
+import gitLogo from "./images/logo-github.png";
+import linkLogo from "./images/logo-linkedin.png";
+import googLogo from "./images/logo-google.png";
+import youLogo from "./images/logo-youtube.png";
 
 function Main() {
     return (
@@ -11,20 +17,34 @@ function Main() {
                     <div id="photo">
                         <img 
                             src={image}
+                            alt="Profile Picture"
+                            id="profilePicture"
                         />
                     </div>
                     <div id="title-block">
-                        <p>Deren Bozer</p>
-                        <p>Frontend Developer</p>
-                        <p>github.com/DerenB</p>
+                        <p id="tb1">Deren Bozer</p>
+                        <p id="tb2">Frontend Developer</p>
+                        <p id="tb3">github.com/DerenB</p>
                     </div>
                     <div id="button-block">
-                        <button>Email</button>
-                        <button>LinkedIn</button>
+                        <div className="button b1">
+                            <img 
+                                src={scrimlogo}
+                                className="button-logo"
+                            />
+                            <p>Scrimba</p>
+                        </div>
+                        <div className="button b2">
+                            <img 
+                                src={codeLogo}
+                                className="button-logo"
+                            />
+                            Codecademy
+                        </div>
                     </div>
-                    <div id="about-block">
-                        <p>About</p>
-                        <p>
+                    <div id="about-block" className="text-block">
+                        <p className="sec-header">About</p>
+                        <p className="sec-body">
                         Soufflé lemon drops bear claw cookie 
                         toffee chocolate. Pudding cotton candy 
                         carrot cake gingerbread cupcake. 
@@ -34,9 +54,9 @@ function Main() {
                         sweet macaroon apple pie marzipan.
                         </p>
                     </div>
-                    <div id="interest-block">
-                        <p>Interests</p>
-                        <p>
+                    <div id="interest-block" className="text-block">
+                        <p className="sec-header">Interests</p>
+                        <p className="sec-body">
                         Cupcake ipsum dolor sit amet candy. 
                         Caramels sweet roll chupa chups donut 
                         shortbread marzipan pie jelly. Sugar 
@@ -44,12 +64,39 @@ function Main() {
                         cake croissant.
                         </p>
                     </div>
-                    <div id="icons-block">
-                        <p>Icons by:</p>
-                        <a target="_blank" href="https://icons8.com">Icons8</a>
+                    <div id="icons-block" className="text-block">
+                        <p className="sec-header">Icons by:</p>
+                        <p className="sec-body">Website: 
+                            <a target="_blank" href="https://icons8.com">Icons8</a>
+                        </p>
                     </div>
                     <div id="footer">
-                    
+                        <div id="footer-box">
+                            <div id="img1" className="imageSquare">
+                                <img 
+                                    src={gitLogo}
+                                    className="footer-logo"
+                                />
+                            </div>
+                            <div id="img2" className="imageSquare">
+                                <img 
+                                    src={linkLogo}
+                                    className="footer-logo"
+                                />
+                            </div>
+                            <div id="img3" className="imageSquare">
+                                <img 
+                                    src={googLogo}
+                                    className="footer-logo"
+                                />
+                            </div>
+                            <div id="img4" className="imageSquare">
+                                <img 
+                                    src={youLogo}
+                                    className="footer-logo"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
